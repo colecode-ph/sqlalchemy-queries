437659d7f17b257ba71e73bf9e989549a764086d
vagrant <vagrant@vagrant.vm>
2017-07-05 14:38:05 +0000

    Finish and test query #2

--- queries.py
+++ queries.py
@@ -1,4 +1,6 @@
 # This file will use SQLAlchemy to run queries on a sqlite database and print them
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
@@ -14,25 +16,50 @@ DBSession = sessionmaker(bind = engine)
 # creates a sessionmaker object, which establishes a link of
 # communication between our code executions and the engine we created
 session = DBSession()
-# create an instance of the DBSession  object - to make a changeses
+# create an instance of the DBSession  object - to make a changes
 # to the database, we can call a method within the session
 
-print "1. Query all of the puppies and print the results in ascending alphabetical order.\n"
 
-puppyNames = session.query(Puppy.name).order_by(Puppy.name)
-for puppyName in puppyNames:
-    print puppyName.name,
-print "\n"
+def alpha_query():
+    print "1. Query all of the puppies and print the results in ascending alphabetical order.\n"
 
-print "3. Query and print the name and weight of the ten heaviest puppies in descending order by weight.\n"
-puppyNames = session.query(Puppy.name, Puppy.weight).order_by(Puppy.weight.desc()).limit(10)
-for puppyName in puppyNames:
-    print puppyName.name + " - " + str(puppyName.weight) + "lbs"
-print "\n"
+    puppyNames = session.query(Puppy.name).order_by(Puppy.name)
+    for puppyName in puppyNames:
+        print puppyName.name,
+    print "\n"
 
-print "4. Query all puppies grouped by the shelter in which they are staying.\n"
-shelterCounts = (session.query(Shelter.name, func.count(Puppy.name).label("count"))
-                .join(Puppy, Shelter.id == Puppy.shelter_id)
-                .group_by(Shelter.name))
-for shelterName in shelterCounts:
-    print shelterName.name + " - " + str(shelterName.count)
+
+def bday_query():
+    print "2. Query all puppies (and birthdates) that are less than 6 months old organized by the youngest first.\n"
+    today = date.today()
+    sixMonthsAgo = today + relativedelta(months=-6)
+    puppyNames = (session.query(Puppy.name, Puppy.dateOfBirth)
+                 .filter(Puppy.dateOfBirth > sixMonthsAgo)
+                 .order_by(Puppy.dateOfBirth.desc()))
+    for puppyName in puppyNames:
+        print puppyName.name + " - " + str(puppyName.dateOfBirth)
+    print "\n"
+
+
+def weight_query():
+    print "3. Query the name and weight of the ten heaviest puppies in descending order by weight.\n"
+    puppyNames = session.query(Puppy.name, Puppy.weight).order_by(Puppy.weight.desc()).limit(10)
+    for puppyName in puppyNames:
+        print puppyName.name + " - " + str(puppyName.weight) + "lbs"
+    print "\n"
+
+
+def shelter_query():
+    print "4. Query all puppies grouped by the shelter in which they are staying.\n"
+    shelterCounts = (session.query(Shelter.name, func.count(Puppy.name).label("count"))
+                    .join(Puppy, Shelter.id == Puppy.shelter_id)
+                    .group_by(Shelter.name))
+    for shelterName in shelterCounts:
+        print shelterName.name + " - " + str(shelterName.count)
+
+if __name__ == '__main__':
+    alpha_query()
+    bday_query()
+    weight_query()
+    shelter_query()
+    session.close()
